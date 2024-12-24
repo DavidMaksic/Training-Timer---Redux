dd0165ffb7e787bc9a52373a460f10de7e5a2758
DavidMaksic <david.maksic88@gmail.com>
2024-12-24 17:41:33 +0100

    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
    return (
       <Provider store={store}>
          <Main>
-            <BrowserRouter>
+            <BrowserRouter basename="/Training-Timer">
                <Routes>
                   <Route index element={<StartPage />} />
                   <Route path="/presets" element={<Presets />} />
