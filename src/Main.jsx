0d9528e0ec5b68933d6f490267eb218a07677385
DavidMaksic <david.maksic88@gmail.com>
2024-12-27 16:23:15 +0100

    Fix

--- src/Main.jsx
+++ src/Main.jsx
@@ -16,10 +16,12 @@ function Main({ children }) {
                   : 'translate-y-[-60%]'
             }  ${
                location.pathname.includes('/timer')
-                  ? 'sm:w-full sm:h-min sm:top-[64%] '
+                  ? 'sm:w-full sm:h-min sm:top-[64%] overflow-y-visible'
                   : ''
             } ${
-               location.pathname.includes('/finish-screen') ? '' : 'sm:h-full'
+               location.pathname.includes('/finish-screen')
+                  ? 'overflow-y-visible'
+                  : 'sm:h-full'
             }`}
          >
             {location.pathname.includes('/timer') ||
