86694927a8fd0158855da6751c5bcdb8c45c2e3c
DavidMaksic <david.maksic88@gmail.com>
2024-12-27 14:43:30 +0100

    Fixes

--- src/Main.jsx
+++ src/Main.jsx
@@ -10,10 +10,10 @@ function Main({ children }) {
    return (
       <div className="min-h-screen sm:pt-5 bg-gradient-to-br from-gray-800 to-neutral-600 selection:bg-lime-600 selection:text-white text-4xl text-neutral-700">
          <div
-            className={`fixed overflow-y-auto no-scrollbar sm:h-full top-2/4 left-2/4 translate-x-[-50%] translate-y-[-60%] sm:translate-y-[-50%] w-1/3 xl:w-1/2 md:w-4/5 sm:w-11/12 mx-auto pt-2 ${
+            className={`fixed overflow-y-auto no-scrollbar sm:h-full top-2/4 left-2/4 translate-x-[-50%]  sm:translate-y-[-50%] w-1/3 xl:w-1/2 md:w-4/5 sm:w-11/12 mx-auto pt-2 ${
                workouts.length >= 3 && location.pathname.includes('/presets')
-                  ? 'h-full translate-y-[-50%] pb-32 sm:pb-6 pt-4'
-                  : ''
+                  ? 'h-full translate-y-[-49%] pb-32 sm:pb-6 pt-4'
+                  : 'translate-y-[-60%]'
             }  ${
                location.pathname.includes('/timer')
                   ? 'sm:w-full sm:h-min sm:top-[65%]'
